c646808961d80db703adcd20c20dbde422de5766
bama4 <bama4@umbc.edu>
2022-08-25 20:02:32 -0700

    #1 audo_support: renamed mode registers to channel

--- src/audio/audio.h
+++ src/audio/audio.h
@@ -16,27 +16,27 @@ private:
     Gameboy &gb;
 
     /* Registers */
-    ByteRegister mode_one_sweep;    /* NR10 */
-    ByteRegister mode_one_duty;     /* NR11 */
-    ByteRegister mode_one_envelope; /* NR12 */
-    ByteRegister mode_one_freq_lo;  /* NR13 */
-    ByteRegister mode_one_freq_hi;  /* NR14 */
+    ByteRegister channel_one_sweep;    /* NR10 */
+    ByteRegister channel_one_duty;     /* NR11 */
+    ByteRegister channel_one_envelope; /* NR12 */
+    ByteRegister channel_one_freq_lo;  /* NR13 */
+    ByteRegister channel_one_freq_hi;  /* NR14 */
 
-    ByteRegister mode_two_duty;     /* NR21 */
-    ByteRegister mode_two_envelope; /* NR22 */
-    ByteRegister mode_two_freq_lo;  /* NR23 */
-    ByteRegister mode_two_freq_hi;  /* NR24 */
+    ByteRegister channel_two_duty;     /* NR21 */
+    ByteRegister channel_two_envelope; /* NR22 */
+    ByteRegister channel_two_freq_lo;  /* NR23 */
+    ByteRegister channel_two_freq_hi;  /* NR24 */
 
-    ByteRegister mode_three_switch;  /* NR30 */
-    ByteRegister mode_three_length;  /* NR31 */
-    ByteRegister mode_three_level;   /* NR32 */
-    ByteRegister mode_three_freq_lo; /* NR33 */
-    ByteRegister mode_three_freq_hi; /* NR34 */
+    ByteRegister channel_three_switch;  /* NR30 */
+    ByteRegister channel_three_length;  /* NR31 */
+    ByteRegister channel_three_level;   /* NR32 */
+    ByteRegister channel_three_freq_lo; /* NR33 */
+    ByteRegister channel_three_freq_hi; /* NR34 */
 
-    ByteRegister mode_four_length;   /* NR41 */
-    ByteRegister mode_four_envelope; /* NR42 */
-    ByteRegister mode_four_poly_ctr; /* NR43 */
-    ByteRegister mode_four_init;     /* NR44 */
+    ByteRegister channel_four_length;   /* NR41 */
+    ByteRegister channel_four_envelope; /* NR42 */
+    ByteRegister channel_four_poly_ctr; /* NR43 */
+    ByteRegister channel_four_init;     /* NR44 */
 
     ByteRegister channel_ctrl;    /* NR50 */
     ByteRegister select_terminal; /* NR51 */
